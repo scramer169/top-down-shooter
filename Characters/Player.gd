ef1ea79eb07eabc5fcd2c9b30e4b9c6c805e32ed
Asaad Shaikh <spamdud169@gmail.com>
2022-02-01 15:32:53 +0500

    nice

--- Characters/Player.gd
+++ Characters/Player.gd
@@ -5,6 +5,7 @@ enum { MOVE, RELOAD }
 onready var animationPlayer = $AnimationPlayer
 
 var state = MOVE
+var move_speed = 300
 
 func _ready():
 	pass 
@@ -16,13 +17,32 @@ func _physics_process(delta):
 
 
 func move_state(delta):
-	animationPlayer.play("RESET")
+	
+	var move_direction = Vector2.ZERO
+	
+	if Input.is_action_pressed("move_up"):
+		move_direction.y -= 1
+	if Input.is_action_pressed("move_down"):
+		move_direction.y += 1
+	if Input.is_action_pressed("move_left"):
+		move_direction.x -= 1
+	if Input.is_action_pressed("move_right"):
+		move_direction.x += 1
+	
+	move_direction.normalized()
+	
+	move_and_slide(move_direction * move_speed)
+	
+	#animationPlayer.play("RESET")
+	
 	if Input.is_action_just_pressed("reload"):
 		state = RELOAD
 		return
-	
+
+
 func reload_state(delta):
 	animationPlayer.play("reload")
 	
 func reload_end():
 	state = MOVE
+	
